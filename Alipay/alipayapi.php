ca3902697b277f6937015878794099b17a29c77d
zhuchencong <xxx>
2014-09-28 14:02:33 +0800

    ce shi

--- Alipay/alipayapi.php
+++ Alipay/alipayapi.php
@@ -30,11 +30,11 @@ require_once("lib/alipay_submit.class.php");
         $payment_type = "1";
         //必填，不能修改
         //服务器异步通知页面路径
-        $notify_url = "http://www.xxx.com/create_direct_pay_by_user-PHP-UTF-8/notify_url.php";
+        $notify_url = "http://zhuchencong.jsonlin.cn/index.php?s=/Advert/Money/okalpay.html";
         //需http://格式的完整路径，不能加?id=123这类自定义参数
 
         //页面跳转同步通知页面路径
-        $return_url = "http://www.xxx.com/create_direct_pay_by_user-PHP-UTF-8/return_url.php";
+        $return_url = "http://zhuchencong.jsonlin.cn/index.php?s=/Advert/Money/okalpay.html";
         //需http://格式的完整路径，不能加?id=123这类自定义参数，不能写成http://localhost/
 
         //卖家支付宝帐户
